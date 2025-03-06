d8a9ea08f86c8eed42c8f532c9fdeec43c098f0e
yeji-star <sizuku134@gmail.com>
2025-03-06 16:44:03 +0900

    study

--- src/main/java/org/example/Main3.java
+++ src/main/java/org/example/Main3.java
@@ -11,26 +11,18 @@ class Main3 {
         Scanner sc = new Scanner(System.in);
 
         int i = sc.nextInt();
-        int j = sc.nextInt();
-        int k = sc.nextInt();
 
-        if (i % 2 == 0) {
+        if (i < 0) {
+            System.out.println("minus");
+        } else {
+            System.out.println("plus");
+        } if ( i % 2 == 0) {
             System.out.println("even");
         } else {
             System.out.println("odd");
         }
 
-        if (j % 2 == 0) {
-            System.out.println("even");
-        } else {
-            System.out.println("odd");
-        }
 
-        if (k % 2 == 0) {
-            System.out.println("even");
-        } else {
-            System.out.println("odd");
-        }
 
     }
 }
