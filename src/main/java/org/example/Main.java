4db228426a548046292f54eef2e93c4aa0327b46
yeji-star <sizuku134@gmail.com>
2025-03-06 15:05:55 +0900

    study

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,27 @@
 package org.example;
-/* 어떤 형식에 맞추어 시간이 입력될 때, 그대로 출력하는 연습을 해보자.
-
-참고
-scanf( ) 함수는 지정한 형식(format)에 따라 입력받는 함수이다.
-따라서, 입력받고자 하는 형식으로 표현해주면 된다.
-
-예시
-scanf("%d:%d", &h, &m); //콜론(:) 기호를 기준으로 두 수가 각 변수에 저장된다. */
+/*  */
 
 import java.util.Scanner;
+
 import static java.lang.Integer.parseInt;
 
 class Main {
     public static void main(String[] args) {
 
+
         Scanner s = new Scanner(System.in);
 
-        String year[] = s.next().split("\\.");
-        int y = parseInt(year[0]);
-        int m = parseInt(year[1]);
-        int d = parseInt(year[2]);
+        String str=s.nextLine();
+        //변수 str에 입력받은 문자를 저장
+        int len = str.length();
+        // 변수 str의 안에 있는 문자열의 길이를 잼
+        // length는 문자열의 길이를 알고 싶을 때
 
-        System.out.printf("%04d.%02d.%02d", y, m, d);
+        for(int i=0;i<len;i++){ //저장된 내용을 하나하나씩 검사해서 실행하게 만들기
+            System.out.printf("%d",i);
+            System.out.printf("\'%s\'\n",str.charAt(i));
+            //charAt : String으로 저장된 문자열 중에서 한 글자만 선택해 char타입으로 변환해줌
+        }
 
 
     }
