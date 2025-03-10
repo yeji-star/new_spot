067c3b0e8d4076042004e63c49cafa043afc1534
yeji-star <sizuku134@gmail.com>
2025-03-10 11:34:39 +0900

    03_10

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,30 +1,27 @@
 package org.example;
-/*  */
 
 import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
 
-        int a = 10; // 점
+        int[] aa = new int[3]; //-> 다양한 타입 x
+        aa[0] = 11;
+        aa[1] = 22;
+        aa[2] = 33;
+        //철수 0[0]=12; -> 데이터의 의미????
 
-        int[] arr = new int[6]; //1차원 배열 // 선
-        // 왼쪽 상황은 예정, 객체의 모양이 일치.
-        //위와 아래의 차이는 없다. 사용법의 차이만 있다.
-        int[][] arr2 = new int[2][3]; //2차원 배열 // 면
-        //2nd (d는 디멘션의 d) Array
-        //int가 3개씩 2묶음
-        //좌표로 써먹을 수 있음
-
-        arr2[0][0] = 1;
-        arr2[0][1] = 2;
-        arr2[0][2] = 3;
-        arr2[1][0] = 4;
-        arr2[1][1] = 5;
-        arr2[1][2] = 6;
-
-        //다차원 배열 (nd array)
-
+        //사용자 정의 객체를 만들고 싶어 -> 위의 이유 때문에
+        철수 a = new 철수();
+        a.이름 = "김철수";
+        a.나이 = 23;
+        a.키 = 95.3;
 
     }
+}
+
+class 철수 {
+    String 이름;
+    int 나이;
+    double 키;
 }
