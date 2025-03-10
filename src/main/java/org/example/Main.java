d0190ba2d4669d16d8e443556a42d161bfb31423
yeji-star <sizuku134@gmail.com>
2025-03-10 11:43:50 +0900

    03_10

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,18 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        
+
         int[] aa = new int[3]; //-> 다양한 타입 x
         aa[0] = 11;
         aa[1] = 22;
         aa[2] = 33;
         //철수 0[0]=12; -> 데이터의 의미????
+
+        IntArr intArr = new IntArr();
+        intArr.eual = 11;
+        intArr.age = 23;
 
         //사용자 정의 객체를 만들고 싶어 -> 위의 이유 때문에
         철수 a = new 철수();
@@ -18,6 +25,11 @@ class Main {
         a.키 = 95.3;
 
     }
+}
+
+class IntArr {
+    int eual;
+    int age;
 }
 
 class 철수 {
