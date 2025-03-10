d49e0486bbfc1bc4561597121052b36e29fa8d49
yeji-star <sizuku134@gmail.com>
2025-03-10 10:22:36 +0900

    03_10

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,14 +6,16 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        double b = sc.nextDouble();
+        int a= 10; // 점
 
-        if ( b>=50 && b<=60) {
-            System.out.println("win");
-        } else {
-            System.out.println("lose");
-        }
+        int[] arr = new int[6]; //1차원 배열 // 선
+        // 왼쪽 상황은 예정, 객체의 모양이 일치.
+        //위와 아래의 차이는 없다.
+        int[][] arr2= new int[2][3]; //2차원 배열 // 면
+        //2nd (d는 디멘션의 d) Array
+        //int가 3개씩 2묶음
+
+        //다차원 배열 (nd array)
 
 
     }
--- src/main/java/org/example/Main3.java
+++ src/main/java/org/example/Main3.java
@@ -17,33 +17,38 @@ class Main3 {
     public static void main(String[] args) {
 
         // 1.
-        boolean arr1[] = new boolean[]{true, false, true};
+        boolean arr1[] = new boolean[3];
+        arr1[0] = true;
+        arr1[1] = false;
+        arr1[2] = true;
 
-        System.out.println(arr1.length);
-        System.out.println(Arrays.toString(arr1));
-        //엥근데이거배웟든가;
+
+        System.out.printf("%s, %s, %s\n", arr1[0], arr1[1], arr1[2]);
+
+
+        System.out.println("======");
 
         // 2.
-        double arr2[] = new double[]{3.14, 7.77, 11.11};
+        double arr2[] = {3.14, 7.77, 11.11};
 
-        System.out.println(Arrays.toString(arr2));
+        System.out.printf("%.2f, %.2f, %.2f\n", arr2[0],arr2[1],arr2[2]);
 
         // 3. arr3 변수에 [1~10] 연결
         // 반복문 사용
 
-        int arr3[] = new int[10];
+        int i;
+        int[] arr3 = new int[10];
 
-        for (int i = 0; i < arr3.length; i++) { //i에 0을 넣었고, i가 arr3의 길이보다 커지면
+
+        for (i = 0; i < arr3.length; i++) { //i에 0을 넣었고, i가 arr3의 길이보다 커지면
             //0의 +1이 끝남
             //arr3의 용량을 내가 10이라고 넣어놓았고?
-            // 
+            //sout의 arr3[i]을 해도 됨
             arr3[i] = i + 1;
+            int j = arr3[i];
+
+            System.out.printf("%d ", j);
         }
-
-        System.out.println(Arrays.toString(arr3));
-
-        //망했다 왜 된건지 이해가 안된다;
-        //Arrays.toString은 분명 객체를 문자형으로 출력시키는 거겠지??
 
 
 
