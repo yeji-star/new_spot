213db7db3edb4af4756ff47c0e17247f585a5e76
yeji-star <sizuku134@gmail.com>
2025-03-10 16:17:46 +0900

    03_10

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,30 +1,43 @@
 package org.example;
 
 import java.util.Scanner;
-
+//name, inMarried 또한 this를 활용해서 객체 내부의 데이터를 introduce가 쓸 수 있도록
 class Main {
     public static void main(String[] args) { //메인 메소드 (함수)
 
-        사람 a = new 사람();
-        System.out.println(a.age);
-        a.hello();
-        a.introduct();
+       사람 a = new 사람();
+       a.name = "김철수";
+       a.age = 23;
+       a.inMarried = false;
+       a.introduce();
+
+       사람 b = new 사람();
+       b.name = "최영희";
+       b.age = 26;
+       b.inMarried = true;
+       b.introduce();
+
     }
 }
 
-class 사람 { // 필드
-    int age = 22;
-    String name = "김철수";
-    boolean c;
+class 사람 { // 설계도 (필드)
+    String name;
+    int age;
+    boolean inMarried;
 
-    void introduct() {
-        System.out.println("hi");
+    void introduce() {
+        String name = this.name;
+        int age = this.age;
+        boolean inMarried = this.inMarried;
+
+        System.out.println("==자기소개==");
+        System.out.printf("이름 : %s\n", name);
+        System.out.printf("나이 : %d\n", age);
+        System.out.printf("결혼여부 : %b", inMarried);
+        System.out.println("");
     }
 
-    void hello() {//여기 안에 있는 걸 불러서 활용할 수 있다.
-        //메소드는 개수 제한이 없다.
-        System.out.println("안녕하세요");
-    }
+
 }
 
 
