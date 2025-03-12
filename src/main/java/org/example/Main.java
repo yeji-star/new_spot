e90b07dfb378b6f9844390359c28fe27cb38bef1
yeji-star <sizuku134@gmail.com>
2025-03-12 11:15:20 +0900

    03_12

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,50 +1,21 @@
 package org.example;
 
-//문제 - 함수를 실행하면 값을 돌려주도록 만들어주세요.
+// 문제 : 아래와 같이 출력되도록 해주세요.
+// 조건 : `숨쉬다` 기능을 중복하지 말아주세요.
+
 class Main {
     public static void main(String[] args) {
-        int 결과;
-
-        결과 = 계산기1.합(10, 20);
-        System.out.println("결과 : " + 결과);
-        // 출력 => 결과 : 30
-
-        결과 = 계산기1.합(30, 20);
-        System.out.println("결과 : " + 결과);
-        // 출력 => 결과 : 50
-
-        결과 = 계산기1.합(30, 70);
-        System.out.println("결과 : " + 결과);
-        // 출력 => 결과 : 100
-
-        결과 = 계산기1.차(30, 70);
-        System.out.println("결과 : " + 결과);
-        // 출력 => 결과 : -40
-
-        결과 = 계산기1.곱(3, 7);
-        System.out.println("결과 : " + 결과);
-        // 출력 => 결과 : 21
+        사람 a사람 = new 사람();
+        a사람.숨쉬다();
+    }
+}
+class 동물 {
+    void 숨쉬다() {
+        System.out.println("숨쉬다.");
     }
 }
 
-class 계산기1 {
-
-    static int 합(int a, int b) {
-
-        return a + b;
-
-    }
-
-    static int 차(int a, int b) {
-
-        return a - b;
-    }
-
-    static int 곱(int a, int b) {
-
-
-        return a * b;
-    }
+class 사람 extends 동물 {
 
 }
 
