271e5f96e7c812bc00cee564c57de49445160eb5
yeji-star <sizuku134@gmail.com>
2025-03-12 14:47:26 +0900

    03_12

---
+++ src/main/java/org/example/Main3.java
@@ -0,0 +1,48 @@
+package org.example;
+
+// 문제 : 아래와 같이 출력되도록 해주세요.
+// 조건 : `숨쉬다` 기능을 중복하지 말아주세요.
+
+class Main3 {
+    public static void main(String[] args) {
+
+        무기1 a무기 = new 칼1();
+
+        a무기.공격();
+        a무기.방어();
+
+
+    }
+}
+
+interface 무기1 {
+    void 공격();
+
+    void 방어();
+}
+
+interface 무기2 {
+    void 공격();
+
+    void 방어();
+}
+
+
+
+class 칼1 implements 무기1, 무기2 {
+    public void 공격() {
+        System.out.println("칼 공격");  // 형변환 하는 과정에서 이것이 빠져버림
+        //하지만 class 무기 안에도 넣는다면 빠지지 않음
+    }
+
+    public void 방어() {
+        System.out.println("칼 방어");
+    }
+
+}
+
+
+
+
+
+
