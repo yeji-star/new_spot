377d390a9f652d3878326b9a82a3c6ef08935ed1
yeji-star <sizuku134@gmail.com>
2025-03-14 16:50:17 +0900

    03_14

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,28 +1,62 @@
 package org.example;
+// 문제 : 아래가 실행되도록 해주세요.
+// 조건 : 배열을 사용할 수 없습니다.
 
 class Main {
     public static void main(String[] args) {
-        사람 a사람1 = new 사람("김철수", 22);
-        사람 a사람2 = new 사람("철영희", 26);
+        사람인력관리소 a사람인력관리소 = new 사람인력관리소();
 
-        System.out.println(a사람1);
-        System.out.printf("이름 : %s , 나이 : %d\n", a사람1.name, a사람1.age);
-        System.out.println(a사람2);
-        System.out.printf("이름 : %s , 나이 : %d", a사람2.name, a사람2.age);
+        a사람인력관리소.add사람("홍길순", 33);
+        // 나이가 33살인 1번째 사람(홍길순)이 추가되었습니다.
+        a사람인력관리소.add사람("홍길동", 20);
+        // 나이가 20살인 2번째 사람(홍길동)이 추가되었습니다.
+        a사람인력관리소.add사람("임꺽정", 30);
+        // 나이가 30살인 3번째 사람(임꺽정)이 추가되었습니다.
+
+        사람 a사람1 = a사람인력관리소.get사람(1);
+        a사람1.자기소개();
+        // 저는 1번, 홍길순, 33살 입니다.
+
+        사람 a사람2 = a사람인력관리소.get사람(2);
+        a사람2.자기소개();
+        // 저는 2번, 홍길동, 20살 입니다.
+
+        사람 a사람3 = a사람인력관리소.get사람(3);
+        a사람3.자기소개();
+        // 저는 3번, 임꺽정, 30살 입니다.
     }
 }
 
-class 사람 extends Object {
-    String name;
-    int age;
+class 사람인력관리소 {
 
-    @Override //주석이랑 다를 바가 없음
-    public String toString() {
-        return "사람[이름 = " + name + ", 나이 = " + age +"]";
+     int 번호2;
+
+    void add사람(String 이름, int 나이) {
+        사람 a사람 = new 사람();
+        a사람.번호 = this.번호2 + 1;
+        a사람.이름 = 이름;
+        a사람.나이 = 나이;
+        System.out.println("나이가 " + a사람.나이 + "살인 " + a사람.번호 + "번째 사람(" + a사람.이름 + ")이 추가되었습니다.");
+this.번호2 = 번호2;
     }
 
-    사람(String name, int age) {
-        this.name = name;
-        this.age = age;
+    사람 get사람(int 번호) {
+        return new 사람();
     }
+}
+
+class 사람 extends 사람인력관리소 {
+
+    int 번호;
+    int 나이;
+    String 이름;
+
+    void 자기소개() {
+
+
+        System.out.println("저는 " + 번호 + "번, " + 이름 + ", " + 나이 + "살 입니다.");
+
+
+    }
+
 }
