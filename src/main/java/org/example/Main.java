e39fad84ee4a711850d2c459a8065ba154dcd904
yeji-star <sizuku134@gmail.com>
2025-03-14 14:02:11 +0900

    03_14

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,48 +1,85 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 class Main {
     public static void main(String[] args) {
+//생성
 
-        int[] arr = new int[10];
+        //타입설정 Int 객체만 가능
+        ArrayList<Integer> members = new ArrayList<>();
 
-        Article[] articles = new Article[10];
+        //초기 용량 지정 (Capacity가 용량이라는 뜻)
+        ArrayList<Integer> num3 = new ArrayList<>(10);
 
-        Article article1 = new Article();
-        articles[0] = article1;
+        //배열을 넣어 생성
 
-        Article article2 = new Article();
-        articles[1] = article2;
+        ArrayList<Integer> list2 = new ArrayList<>(Arrays.asList(1, 2, 3));
 
-        System.out.println(articles[0].id);
-        System.out.println(articles[1].id);
+        //다른 컬렉션으로부터 그대로 요소를 받아서 생성
+        //ArrayList를 인자로 받는 API를 사용하기 위해 Collection 타입 변환이필요할 떄 많이 사용
+        ArrayList<Integer> list3 = new ArrayList<>(list2);
+        //얘는 위에 있는 걸 가져왔네
 
-        System.out.println(article1.id);
-        System.out.println(article2.id);
+        //추가
+        ArrayList<String> list = new ArrayList<>(10);
+
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("D");
+
+
+        list.size(); //크기(size)는 6
+
+        ArrayList<String> list02 = new ArrayList<>();
+        list02.add("E");
+        list02.add("F");
+
+        list02.addAll(list);
+
+        System.out.println(list02);
+
+        System.out.println("== 삭제 ==");
+
+        ArrayList<String> list01 = new ArrayList<>(8);
+
+        list01.add("1");
+        list01.add("2");
+        list01.add("3");
+        list01.add("4");
+        list01.add("5");
+
+        list01.remove(2); // 2번째 자리의 요소 삭제
+
+        System.out.println(list01);
+
+        System.out.println("== 그냥 말끔히 삭제 ==");
+
+        list01.clear();
+
+        System.out.println(list01);
+
+        System.out.println("== 얻기 ==");
+
+        ArrayList<String> getlist = new ArrayList<>(18);
+
+        getlist.add("1");
+        getlist.add("2");
+        getlist.add("3");
+        getlist.add("4");
+        getlist.add("5");
+
+        getlist.get(2);
+        getlist.get(4);
+
+        System.out.println("== 범위를 get ==");
+
+        ArrayList<String> Morelist = new ArrayList<>(18);
 
     }
 }
 
-class Article {
-    static int lastid;
-    int id;
-    String rg;
 
-    static {
-        lastid = 0;
-    }
 
-    Article() {
-        this(lastid + 1, "2025"); //아래의 아티클 this로 들어간다
-        //다른 생산자 호출, 실행
-        lastid++;
-
-    }
-
-    Article(int id, String rg) {
-        this.id = id;
-        this.rg = rg;
-    }
-
-}
-
-//CRUD = C <-글쓰기 R <-상세보기, 글읽기, 검색 U <- 글수정 D <- 글삭제
