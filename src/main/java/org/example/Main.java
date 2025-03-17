1ea229fc31ccaa4303e8308e1d3a64cce49446d9
yeji-star <sizuku134@gmail.com>
2025-03-17 11:42:28 +0900

    03_17

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,84 +1,32 @@
 package org.example;
 
-// 문제 : 아래가 실행되도록 해주세요.
-// 조건 : 배열을 사용할 수 없습니다.
 
-/* class Main {
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+class Main {
     public static void main(String[] args) {
-        사람인력관리소1 a사람인력관리소 = new 사람인력관리소1();
 
-        a사람인력관리소.add사람("홍길순", 33);
-        // 나이가 33살인 1번째 사람(홍길순)이 추가되었습니다.
-        a사람인력관리소.add사람("홍길동", 20);
-        // 나이가 20살인 2번째 사람(홍길동)이 추가되었습니다.
-        a사람인력관리소.add사람("임꺽정", 30);
-        // 나이가 30살인 3번째 사람(임꺽정)이 추가되었습니다.
+        Scanner sc = new Scanner(System.in);
 
-        사람1 a사람1 = a사람인력관리소.get사람(1);
-        a사람1.자기소개();
-        // 저는 1번, 홍길순, 33살 입니다.
+        int a = 0;
 
-        사람1 a사람2 = a사람인력관리소.get사람(2);
-        a사람2.자기소개();
-        // 저는 2번, 홍길동, 20살 입니다.
-
-        사람1 a사람3 = a사람인력관리소.get사람(3);
-        a사람3.자기소개();
-        // 저는 3번, 임꺽정, 30살 입니다.
-    }
-}
-
-class 사람인력관리소1 {
-    사람1 a사람1;
-    사람1 a사람2;
-    사람1 a사람3;
-
-    int 숫자추가 = 0;
-
-    void add사람(String 이름, int 나이) {
-
-        int 번호 = 숫자추가 + 1;
-
-        사람 a사람 = new 사람();
-        a사람.이름 = 이름;
-        a사람.나이 = 나이;
-        a사람.번호 = 번호;
-
-        if (번호 == 1) {
-            a사람1 = a사람1;
-        } else if (번호 == 2) {
-            a사람2 = a사람1;
-        } else if (번호 == 3) {
-            a사람3 = a사람1;
+        while (true) {
+            try {
+                System.out.printf("숫자 : ");
+                a = sc.nextInt();
+                sc.nextLine(); // 버퍼를 지운다
+                break;
+            } catch (InputMismatchException e){
+                System.out.println("숫자 틀림");
+                sc.nextLine();
+            }
         }
-        System.out.println("나이가 " + a사람.나이 + "살인 " + a사람.번호 + "번째 사람(" + a사람.이름 + ")이 추가되었습니다.");
-        숫자추가++;
 
 
-    }
+        System.out.println("입력된 숫자 : " + a);
 
-    사람1 get사람(int 번호) {
-        if (번호 == 1) {
-            return a사람1;
-        } else if (번호 == 2) {
-            return a사람2;
-        } else if (번호 == 3) {
-            return a사람3;
-        }
-        return null;
-    }
+        sc.close();
 
-}
-
-class 사람1 {
-    String 이름;
-    int 나이;
-    int 번호;
-
-    void 자기소개() {
-        System.out.println("저는 " + 번호 + "번, " + 이름 + ", " + 나이 + "살 입니다.");
     }
 }
-
-
- */
