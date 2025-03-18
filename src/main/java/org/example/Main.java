253077179dedc4e4b3863a87f6965b5e4db9665d
yeji-star <sizuku134@gmail.com>
2025-03-18 09:35:52 +0900

    03_18

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,36 @@
 package org.example;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
+
+import static java.util.Arrays.stream;
 
 class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        String[] input = sc.nextLine().split(" ");
+        long sum = Arrays.stream(sc.nextLine().split(" "))
+                //스트림 사용시 이렇게 줄일 수도 있음
+                .mapToLong(e -> Long.parseLong(e))
+                .sum();
 
-        long a = Long.parseLong(input[0]);
-        long b = Long.parseLong(input[1]);
-        long c = Long.parseLong(input[2]);
+//        String[] inputBits = sc.nextLine().split(" "); //
 
-        System.out.println(a + b + c);
+        //        long sum = Arrays.stream(inputBits) // 스트림 사용함
+//                .mapToLong(e -> Long.parseLong(e))
+//                .sum();
+
+//        long sum = 0; // 스트림 사용안함
+//
+//        for (String inputBit : inputBits) { // 스트림 사용안함
+//            sum += Long.parseLong(inputBit);
+//        }
+
+
+
+        System.out.println(sum);
 
     }
 }
